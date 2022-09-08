3bca43d47bcca9ccc866d5e27e038a9c0062911f
and078 <34898237+and078@users.noreply.github.com>
2022-09-08 09:57:12 +0300

    some refactoring

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-const usd = document.getElementById('usd')
-const ron = document.getElementById('ron')
-const rub = document.getElementById('rub')
-const uah = document.getElementById('uah')
-const gbp = document.getElementById('gbp')
-const eur = document.getElementById('eur')
-//usd.textContent = 'usd'
 const someDate = 1661900400000
 const msPerDay = 86400000
 let now = Date.now()
-let start = (((now - someDate) / msPerDay).toFixed(0)) * msPerDay + someDate
+let start = (Math.trunc((now - someDate) / msPerDay)) * msPerDay + someDate
 
 const url = `https://point.md/curs/methods/money/newrates/?start=${start}&stop=${start}`
 
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        usd.textContent = data.usd[0][1]
-        ron.textContent = data.ron[0][1]
-        rub.textContent = data.rub[0][1]
-        uah.textContent = data.uah[0][1]
-        gbp.textContent = data.gbp[0][1]
-        eur.textContent = data.eur[0][1]
+        ['usd', 'ron', 'rub', 'uah', 'gbp', 'eur'].forEach((c) => {
+            document.getElementById(c).textContent = data[c][0][1]
+        })
     });
-
