23265e11cf9c92522800c808bf2c1dedda4ff493
and078 <34898237+and078@users.noreply.github.com>
2022-09-20 11:16:24 +0300

    add onChange event

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
+import fn from "./modules/diagramm.js"
+
+fn()
+
 const someDate = 82800000
 const msPerDay = 86400000
 let start = (Math.trunc((Date.now() - someDate) / msPerDay)) * msPerDay + someDate
+const currenciesInMDL = {}
+document.getElementById('date').textContent = new Date(Date.now()).toLocaleDateString()
+document.getElementById('mdl').value = 100
+
+const inputs = document.querySelectorAll('.form-control')
 
 const url = `https://point.md/curs/methods/money/newrates/?start=${start}&stop=${start}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        ['usd', 'ron', 'rub', 'uah', 'gbp', 'eur'].forEach((c) => {
-            document.getElementById(c).textContent = data[c][0][1]
+async function fetchUrl(url){
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            Reflect.ownKeys(data).forEach(c => {
+                currenciesInMDL[c] = data[c][0][1]
+            })
+            currenciesInMDL['mdl'] = 1
         })
-    });
+    initFieldsFill()    
+}
+
+fetchUrl(url)
+
+const initFieldsFill = () => {
+    inputs.forEach(el => {
+        document.getElementById(el.id).value = +((document.getElementById('mdl').value) * (currenciesInMDL['mdl'] / currenciesInMDL[el.id])).toFixed(4)
+    })
+}
+
+const onChangeAnyInput = (currentCurrency) => {
+    let currentInp = +(document.getElementById(currentCurrency.id).value)
+    inputs.forEach(el => {
+        document.getElementById(el.id).value = +(currentInp * (currenciesInMDL[currentCurrency.id] / currenciesInMDL[el.id])).toFixed(4)
+    })
+}
+
+inputs.forEach( el => el.addEventListener('input', () => {
+    onChangeAnyInput(el)
+}))
